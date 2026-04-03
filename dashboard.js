4a3b5d5a447abbd7dfa3e93563a40988bc79bb05
ItzAlexR0blx <155121437+lucasfox5@users.noreply.github.com>
2026-04-04 00:15:52 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,6 @@
 async function loadDashboard() {
     if (localStorage.getItem("loggedIn") !== "true") {
-        return window.location.href = "/login.html";
+        return window.location.href = "login.html";
     }
 
     const userId = localStorage.getItem("userId");
@@ -8,15 +8,17 @@ async function loadDashboard() {
 
     document.getElementById("name").innerText = username;
 
-    const avatarRes = await fetch(`https://thumbnails.roblox.com/v1/users/avatar?userIds=${userId}&size=150x150&format=Png`);
-    const avatarData = await avatarRes.json();
+    const avatarRes = await fetch(
+        `https://thumbnails.roblox.com/v1/users/avatar?userIds=${userId}&size=150x150&format=Png`
+    );
 
+    const avatarData = await avatarRes.json();
     document.getElementById("avatar").src = avatarData.data[0].imageUrl;
 }
 
 function logout() {
     localStorage.clear();
-    window.location.href = "/";
+    window.location.href = "index.html";
 }
 
 loadDashboard();
