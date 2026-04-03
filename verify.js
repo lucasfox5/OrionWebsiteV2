4ca1aaa43178b45ee40059a1305b50a455668c5f
ItzAlexR0blx <155121437+lucasfox5@users.noreply.github.com>
2026-04-04 00:14:52 +0100

    Update verify.js

--- verify.js
+++ verify.js
@@ -3,10 +3,12 @@ function generateCode() {
     if (!username) return alert("Enter a username");
 
     const code = Math.random().toString(36).substring(2, 10);
+
     localStorage.setItem("verifyCode", code);
     localStorage.setItem("verifyUser", username);
 
     document.getElementById("codeDisplay").innerHTML =
-        `Put this code in your Roblox bio:<br><b>${code}</b><br><br>
-        After adding it, click <a href="check.html">Verify</a>`;
+        `Put this code in your Roblox bio:<br><br>
+        <b>${code}</b><br><br>
+        After adding it, click <a href='check.html'>Verify</a>`;
 }
